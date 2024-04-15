7b3335fcc18f71fc02dd31b3977ea329ca8710d7
santilapi13 <santilapiana02@gmail.com>
2024-04-15 19:05:36 -0300

    Lógica de torres terminada

--- prefabs/Enemy.gd
+++ prefabs/Enemy.gd
@@ -11,7 +11,8 @@ func _ready():
 	get_tree().get_nodes_in_group("target")[0].connect("body_entered", reach_target)
 	velocity = Vector2.ZERO
 	target = get_parent().get_parent().get_node("Target")
-
+	nav.target_position = target.position
+	
 func _physics_process(delta):
 	move(delta)
 		
@@ -20,16 +21,24 @@ func _physics_process(delta):
 func move(delta):
 	var direction = Vector3()
 	
-	nav.target_position = target.position
-	
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
 	
 	velocity = velocity.lerp(direction * speed, acceleration * delta)
 	
 	move_and_slide()
+	
+func get_next_waypoint():
+	return nav.get_next_path_position()
+	
+func get_current_waypoint_index():
+	return nav.get_current_navigation_path_index()
 
 func reach_target(body):
 	if body == self:
 		queue_free()
 		print("Un enemigo ha escapado")
+		
+func take_damage():
+	set_process_mode(Node.ProcessMode.PROCESS_MODE_DISABLED)
+	#visible = false
--- prefabs/tower.gd
+++ prefabs/tower.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var range
 var fire_rate = 1
+var cooldown = 0
 var damage
 
 var bullet_prefab
@@ -10,13 +11,15 @@ var current_target
 
 var enemies_in_range = []
 
-func _ready():
-	get_node("Timer").wait_time = fire_rate
-
-
-func _on_timer_timeout():
+func _process(delta):
+	if cooldown > 0:
+		cooldown -= delta
+		return
+	
+	
 	if current_target != null and current_target in enemies_in_range:
 		shoot(current_target)
+		cooldown = fire_rate
 	
 	set_new_target()
 	
@@ -24,32 +27,29 @@ func _on_timer_timeout():
 func add_enemy(body):
 	if body != self:  # Agregar que sea del grupo enemy
 		enemies_in_range.append(body)
-		print("Objeto entró en la esfera:", body.name)
 
 # Función para detectar cuando un objeto sale del área
 func delete_enemy(body):
 	if body != self and body in enemies_in_range:
 		enemies_in_range.erase(body)
-		print("Objeto salió de la esfera:", body.name)
 
 func shoot(target):
-	pass
+	# TODO: Hacer las demás cosas como ver la probabilidad de acierto, etc.
+	target.take_damage()
 	
-# TODO: Arreglar tema waypoints (ver comentarios)
 func set_new_target():
 	var first_enemy = null
-	var first_enemy_distance_left = Mathf.Infinity  # Ver como inicializar en infinito
+	var first_enemy_distance_left = INF
 	for enemy in enemies_in_range:
-		var enemy_distance_left = Vector2.Distance(enemy.transform.position, enemy.nextWaypoint.position) # Arreglar esto
+		var enemy_distance_left = enemy.position.distance_to(enemy.get_next_waypoint())
 		
 		if first_enemy != null:
-			firstEnemyDistanceLeft = Vector2.Distance(first_enemy.transform.position, first_enemy.nextWaypoint.position) # Arreglar esto
+			first_enemy_distance_left = first_enemy.position.distance_to(first_enemy.get_next_waypoint())
 		else:
 			first_enemy = enemy
 			continue  # Ver si continue funciona como en C#
 		
 		# Arreglar esto
-		if enemy.nextWaypointIndex > first_enemy.nextWaypointIndex or (enemy.nextWaypointIndex == first_enemy.nextWaypointIndex and enemy_distance_left < first_enemy_distance_left):
+		if enemy.get_current_waypoint_index() > first_enemy.get_current_waypoint_index() or (enemy.get_current_waypoint_index() == first_enemy.get_current_waypoint_index() and enemy_distance_left < first_enemy_distance_left):
 			first_enemy = enemy;
-	
 	current_target = first_enemy
