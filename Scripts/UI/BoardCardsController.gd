74c4caec784e1154ff1b939e6e6d78db4809072d
artizan3 <129905900+artizan3@users.noreply.github.com>
2024-04-17 10:06:22 -0300

    Linkeado

--- Scripts/UI/BoardCardsController.gd
+++ Scripts/UI/BoardCardsController.gd
@@ -1,25 +1,16 @@
-extends Node
+extends BaseDeck
 
 @export var m_cardMovement : Node
 @export var m_cardsAmount : int = 5
 @export var m_panels: Array[Panel] = []
 @export var m_locationSpeed : float = 10.0
-@export var m_hideBoardRef : Control = null
-@export var m_boardRef : Control = null
-var f_state = null
-var m_cardsList = []
-var m_hide:bool = true
 
 
 func _ready():
-	GameEvents.OnLoadBoard_AddListener(LoadBoard)
-
-func _process(delta):
-	if (f_state != null):
-		f_state.call(delta)
+	super._ready()
+	GameEvents.OnLoadBoard.AddListener(LoadBoard)
 
 #STATES
-
 func State_CardsLocation(delta):
 	var ending = false
 	for i in range(len(m_cardsList)):
@@ -33,31 +24,22 @@ func State_CardsLocation(delta):
 				ending = true
 	if (ending):
 			f_state = null
-
 #END
 
 func LoadBoard(cardsList):
 	for c in m_cardsList:
 		c.SetInputEvent(null)	
-	GameEvents.OnLoadDiscard_Call(m_cardsList)
+	GameEvents.OnLoadDiscard.Call(m_cardsList)
 	
-	m_cardsList = cardsList	
+	m_cardsList = cardsList
 	for i in range(len(m_cardsList)):
 		var p = m_panels[i]
 		var c = m_cardsList[i]
 		c.SetInputEvent(GetInputEvent)
+		c.SetSide(true)
 		
 	f_state = State_CardsLocation
 
 func GetInputEvent(card, event):
 	m_cardMovement.GetInputEvent(card, event)
 
-func _on_button_hide_board_cards_button_down():
-	if (m_hide==false):
-		m_boardRef.anchor_top=0
-		m_boardRef.anchor_bottom=1
-		m_hide=true
-	else:
-		m_boardRef.anchor_top=m_hideBoardRef.anchor_top
-		m_boardRef.anchor_bottom=m_hideBoardRef.anchor_bottom
-		m_hide=false
--- Scripts/UI/DeckCardsController.gd
+++ Scripts/UI/DeckCardsController.gd
@@ -17,6 +17,6 @@ func _on_button_button_up():
 
 func _on_button_2_button_up():	
 	var newList = RemoveCards(5)
-	GameEvents.OnLoadBoard_Call(newList)
+	GameEvents.OnLoadBoard.Call(newList)
 	m_actualCardIndex = 0
 	f_state = State_LoadCards
