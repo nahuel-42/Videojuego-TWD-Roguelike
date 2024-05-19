ef8824ea0af6ad28be96dfd2a18d3e95378b2763
santilapi13 <santilapiana02@gmail.com>
2024-05-19 10:35:00 -0300

    Fix de missing scripts

---
+++ Scripts/enemies_spawner.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+const ENEMY_PREFAB_PATH = "res://Prefabs/Enemies/Enemy.tscn"
+
+var last_enemy_index
+var enemies = []
+var enemies_node 
+
+func _ready():
+	enemies_node = $Enemies
+	#load_enemies()
+
+func load_enemies():
+	var prefab = load(ENEMY_PREFAB_PATH)
+	enemies = []
+	last_enemy_index = 0
+	for i in range(10):  # Agrega 10 enemigos de ejemplo
+		var enemy_instance = prefab.instantiate()
+		enemy_instance.add_to_group("Beast" if i % 2 == 0 else "Soldier")
+		
+		enemies_node.add_child(enemy_instance)
+		enemies.append(enemy_instance)
+		enemy_instance.set_process_mode(Node.ProcessMode.PROCESS_MODE_DISABLED)
+		enemy_instance.visible = false
+
+func reset_enemies():
+	for enemy in enemies:
+		if enemy.get_process_mode() != Node.ProcessMode.PROCESS_MODE_DISABLED:
+			return
+			
+	print("Resetting enemies...")
+	load_enemies()
+	
+
+func _on_timer_timeout():
+	if len(enemies) == 0:
+		load_enemies()
+	
+	if last_enemy_index < len(enemies):
+		enemies[last_enemy_index].set_process_mode(Node.ProcessMode.PROCESS_MODE_INHERIT)
+		enemies[last_enemy_index].visible = true
+		last_enemy_index += 1
+	else:
+		reset_enemies()
