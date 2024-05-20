2a3d60957b37239d36260ff28a9b71d5cb7a4c3b
artizan3 <brunotrinitario@gmail.com>
2024-05-20 16:16:51 -0300

    update global card list

--- Scripts/Card/Persistence/GlobalCardsList.gd
+++ Scripts/Card/Persistence/GlobalCardsList.gd
@@ -8,13 +8,15 @@ extends Node
 
 var CollectionCard = [
 	################# TOWERS ####################
-	{"id":0, "type": "tower", "cardName" : "Archer Tower", "desc":"Data", "sprite": "res://Assets/Sprites/Heroes/Rogue/Idle/Idle-Sheet.png", "cost":100, "range": 150, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':'res://prefabs/archer_tower.tscn'},
-	{"id":1, "type": "tower", "cardName" : "Area Tower", "desc":"Data", "sprite": "res://Assets/Sprites/Heroes/Wizzard/Idle/Idle-Sheet.png", "cost":100, "range": 125, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':'res://prefabs/area_tower.tscn'},
-	{"id":2, "type": "tower", "cardName" : "Fire Tower", "desc":"Data", "sprite":"res://Assets/Fire1.png", "cost":100, "range": 20, "damage":20, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':'res://X.tscn'},
-	{"id":3, "type": "tower", "cardName" : "Fire Tower2", "desc":"Data", "sprite":"res://Assets/Fire2.png", "cost":100, "range": 20, "damage":20, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':'res://X.tscn'},
-	{"id":4, "type": "tower", "cardName" : "Fire Tower3", "desc":"Data", "sprite":"url1", "cost":100, "range": 20, "damage":20, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 0, 'path':'res://X.tscn'}
-	################# Activate Card ####################
-	################# POWER ####################
+	{"id":0, "type": "tower", "cardName" : "Fire Archer Tower", "desc":"", "sprite": "res://Assets/Sprites/Heroes/Rogue/Idle/Idle-Sheet.png", "cost":100, "range": 150, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':'res://prefabs/archer_tower.tscn'},
+	{"id":1, "type": "tower", "cardName" : "Fire Area Tower", "desc":"", "sprite": "res://Assets/Sprites/Heroes/Wizzard/Idle/Idle-Sheet.png", "cost":100, "range": 125, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':'res://prefabs/area_tower.tscn'},
+	{"id":2, "type": "tower", "cardName" : "Fire Bomb Tower", "desc":"", "sprite": "", "cost":100, "range": 150, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':''},
+	{"id":3, "type": "tower", "cardName" : "Ice Archer Tower", "desc":"", "sprite": "", "cost":100, "range": 125, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':''},
+	{"id":4, "type": "tower", "cardName" : "Ice Area Tower", "desc":"", "sprite": "", "cost":100, "range": 150, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':''},
+	{"id":5, "type": "tower", "cardName" : "Ice Bomb Tower", "desc":"", "sprite": "", "cost":100, "range": 125, "damage": 1, "attackSpeed": 1, "accuracy": 0.1, "unlocked": 1, 'path':''},
+	################# Upgrade Card #############
+	
+	################# Activate Card #############
 	################# Spell ####################
 	################# Camp ####################
 ]
