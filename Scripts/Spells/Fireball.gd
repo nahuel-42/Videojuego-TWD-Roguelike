54d5f683fc6eb387a0e0bea026e9089946829cd6
santilapi13 <santilapiana02@gmail.com>
2024-05-30 13:14:10 -0300

    Poderes: fireball bugeada agregada al mapa

--- Scripts/Spells/Fireball.gd
+++ Scripts/Spells/Fireball.gd
@@ -1,27 +1,34 @@
-extends CharacterBody2D
+extends Area2D
 class_name Fireball
 
 var destination : Vector2
+var velocity : Vector2
+var damage : int = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CollisionShape2D.set_process(false)
-	position = - get_viewport_rect().size / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_and_slide()
-	print("position: ", position)
-	print("destination: ", destination)
-	print("distance: ", position.distance_to(destination))
+	move(delta)
 	if(position.distance_to(destination) < 10):
 		print("BOOM!")
 		$CollisionShape2D.set_process(true)	
+		velocity = Vector2.ZERO
+		queue_free()
 
-func load_stats(animation_destination : Vector2):
-	destination = animation_destination - get_viewport_rect().size / 2
-	print("voy a arrancar desde ", position)
-	print("el destino es ", destination)
-	velocity = animation_destination.normalized() * 200
-	print("la velocidad es de ", velocity)
+func load_stats(animation_destination : Vector2, cell_dimension):
+	position = animation_destination - Vector2(cell_dimension, 3 * cell_dimension)
+	destination = animation_destination
+	velocity = (position + animation_destination).normalized() * 200
+
+func move(delta):
+	position += velocity * delta
+
+func perform(body):
+	body.take_damage(damage)
+
+func _on_enemy_entered(body):
+	perform(body)
--- Scripts/spell_detector.gd
+++ Scripts/spell_detector.gd
@@ -2,11 +2,14 @@ extends Node2D
 class_name SpellDetector
 
 const FIREBALL_PREFAB = "res://Prefabs/Spells/Fireball.tscn"
+var size : Vector2
+@onready var map = $"../TileMap"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Area2D/CollisionShape2D.shape.size = get_viewport().size
-	position = get_viewport_rect().size /2
+	size = Vector2(map.width * map.CELL_DIMENSION, map.height * map.CELL_DIMENSION)
+	$Area2D/CollisionShape2D.shape.size = size
+	position = size / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,8 +21,6 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 		var prefab = load(FIREBALL_PREFAB)
 		var spell = prefab.instantiate()
 		add_child(spell)
-		spell.load_stats(event.position)
+		spell.load_stats(event.global_position, map.CELL_DIMENSION)
+		print("Se clickeo en: " + str(event.global_position))
 
-func _on_area_2d_body_entered(body):
-	print("Enemy detected")
-	$Fireball.set_process(false)
