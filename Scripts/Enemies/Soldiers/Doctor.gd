04516dc08318aecb2d4a3cd0bcbbff2c1ac084f2
Tomi Bedini <bedinitomas@gmail.com>
2024-06-04 21:28:58 -0300

    Sprites agregados

--- Scripts/Enemies/Soldiers/Doctor.gd
+++ Scripts/Enemies/Soldiers/Doctor.gd
@@ -2,11 +2,18 @@ extends Soldier
 class_name EnemyDoctor
 
 #TODO: curar enemigos que estan en rango
-var healing_speed = 2 # TODO: cambiar esto pq estaria muy roto
+var cooldown_heal = 2 # TODO: cambiar esto pq estaria muy roto
 var amount_heal = 1
 var allies_in_range = []
 
 @onready var animation_doctor = $AnimationDoctor
+
+func perform(delta): #Ver como hacer esto despues
+	if cooldown_heal > 0:
+		
+		return
+
+	healAlly()
 
 func add_ally(body):
 	if body.is_in_group("SOLDIER") and body != self:
@@ -16,5 +23,9 @@ func delete_ally(body):
 	if body in allies_in_range and body != self:
 		allies_in_range.erase(body)
 
-func heal():
-	pass
+func healAlly():
+	if len(allies_in_range) != 0:
+		cooldown_heal = 1 #Modificar !!!!
+	for ally in allies_in_range:
+		ally.heal(amount_heal) #Crear Funcion en enemy que lo permita curarse
+		
