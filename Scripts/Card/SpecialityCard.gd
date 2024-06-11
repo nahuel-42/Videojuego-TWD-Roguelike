beff8d9b840653978f60a00ce1d11c5f56e16f2c
panchogb <franciscogbmdp@gmail.com>
2024-06-11 16:28:06 -0300

    fix slotDetector

--- Scripts/Card/SpecialityCard.gd
+++ Scripts/Card/SpecialityCard.gd
@@ -2,6 +2,7 @@ class_name SpecialityCard
 extends ActiveCard
 
 var m_slot = null
+var m_specialityType : int = -1
 
 func use(param):
 	print("Se uso SpecialityCard")
@@ -10,21 +11,15 @@ func use(param):
 	
 	var id = m_slot.check_speciality(m_idCard)
 	if (id != -1):
-		GameEvents.OnShowPopupSpeciality.Call([self])
+		GameEvents.OnShowPopupSpeciality.Call([self, id])
 		GameEvents.OnRemoveBoardCards.Call([[card]])
 		GameEvents.OnLoadDiscard.Call([[card]])
 		return true
 	return false
 	#GameEvents.OnLoadDiscard(param)
 
-func UseSpeciality1():
-	pass
-
-func UseSpeciality2():
-	pass
-
-func UseSpeciality3():
-	pass
+func UseSpeciality(type : int):
+	m_slot.apply_card(type)
 	
 func SetTypeDetector(cardMovement : CardMovement):
 	cardMovement.SetSpecialityDetector()
