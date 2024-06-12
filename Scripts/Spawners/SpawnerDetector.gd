3983927b25022525b291cba1a992bb723f30852c
panchogb <franciscogbmdp@gmail.com>
2024-06-11 21:07:30 -0300

    fix sprite spawner

--- Scripts/Spawners/SpawnerDetector.gd
+++ Scripts/Spawners/SpawnerDetector.gd
@@ -5,11 +5,9 @@ extends Node
 @export var m_sprite : Sprite2D = null
 
 func glow_slot():
-	m_sprite = $Sprite2D
 	m_sprite.modulate = Color(255,255,34)
 	
 func unglow_slot():
-	m_sprite = $Sprite2D
 	m_sprite.modulate = Color(1, 1, 1)
 	
 func GetSpawner():
