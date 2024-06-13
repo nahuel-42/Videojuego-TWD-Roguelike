111c458ff65ed96ab6840370e83de5d1cabc50bc
Bruno Trinitario <brunotrinitario@gmail.com>
2024-06-13 20:11:42 -0300

    intentando hacer gameover

--- Scenes/gameOver.gd
+++ Scenes/gameOver.gd
@@ -1,10 +1,10 @@
-extends Control
-
-var dir_main_menu=load("res://Scenes/Menu/menuPrincipal.tscn")
+extends Node2D
+@export var m_self : Node2D = null
 
 func _on_button_button_down():
-	get_tree().change_scene_to_packed(dir_main_menu) 
+	var dir_main_menu=load("res://Scenes/Menu/menuPrincipal.tscn")
+	get_node("/root").free()
+	get_node("/root").add_child(dir_main_menu)
+	#get_tree().paused=false
+	#get_tree().change_scene_to_packed(dir_main_menu)
 
-
-func _on_button_down():
-	get_tree().change_scene_to_packed(dir_main_menu) 
