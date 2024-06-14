8546430517eba08a9e873d42d8ea2dfb42e1c7e3
MicaR <micaelarasso99@gmail.com>
2024-06-14 14:30:40 -0300

    Menu desde game over

--- Scenes/gameOver.gd
+++ Scenes/gameOver.gd
@@ -1,10 +1,12 @@
 extends Node2D
 @export var m_self : Node2D = null
 
+
 func _on_button_button_down():
 	var dir_main_menu=load("res://Scenes/Menu/menuPrincipal.tscn")
-	get_node("/root").free()
-	get_node("/root").add_child(dir_main_menu)
+#	get_node("/root").free()
+#	get_node("/root").add_child(dir_main_menu)
+	get_tree().change_scene_to_packed(dir_main_menu)
 	#get_tree().paused=false
 	#get_tree().change_scene_to_packed(dir_main_menu)
 
