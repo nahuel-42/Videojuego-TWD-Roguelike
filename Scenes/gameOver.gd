3ac0a4a514a41ccaa8642e6f298a2b1d08446cd9
MicaR <micaelarasso99@gmail.com>
2024-06-16 23:39:05 -0300

    Botones

--- Scenes/gameOver.gd
+++ Scenes/gameOver.gd
@@ -9,13 +9,21 @@ func _ready():
 	create_tween().tween_property($Sign, "position", SignPosition, 1)
 	create_tween().tween_property($Background, "modulate", SignColor, 1)
 	
-func _on_button_button_down():
-	get_tree().paused=false
-	get_tree().change_scene_to_packed(dir_main_menu)
+#func _on_button_button_down():
+#	get_tree().paused=false
+#	get_tree().change_scene_to_packed(dir_main_menu)
 
 
-func _on_main_Resurrect():
-	get_tree().paused=false
-	GameController.Resurrect()
-	GameEvents.OnSetVisible.Call([true])
-	queue_free()
+#func _on_main_Resurrect():
+#	get_tree().paused=false
+#	GameController.Resurrect()
+#	GameEvents.OnSetVisible.Call([true])
+#	queue_free()
+
+
+func _on_main_menu_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_main_menu_pressed():
+	pass # Replace with function body.
